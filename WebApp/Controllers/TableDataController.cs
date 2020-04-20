58ad876626cd8595f8c288b618f728d6e1e119f5
Vasily Himchenko <White_Scorpion@inbox.lv>
2020-04-20 10:49:45 +0300

    Data view initial.

--- WebApp/Controllers/TableDataController.cs
+++ WebApp/Controllers/TableDataController.cs
@@ -1,17 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
+using WebApp.Models.Data;
+using WebApp.Models.Reps;
 
 namespace WebApp.Controllers
 {
     public class TableDataController : Controller
     {
+        private static IRepSimple<long, ProgramDbTableRow> _rep;
+
+        public TableDataController()
+        {
+            if(_rep == null)
+                _rep = new RepTable();
+        }
+
         // GET: TableData
         public ActionResult Index()
         {
-            return View();
+            return View(_rep.Get());
         }
     }
 }
